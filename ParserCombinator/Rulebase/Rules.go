2f1e254188f85eefb6d5d068926137805b4589f6
John Antypas <ja@antypas.net>
2025-07-16 06:56:16 -0700

    Added test cases to Lexer

---
+++ ParserCombinator/Rulebase/Rules.go
@@ -0,0 +1,118 @@
+package Rulebase
+
+import "ParserCombinator/ParserCombinator/ParserCore"
+
+type DataObject struct {
+	Command   string // The command to execute, e.g., "MOVE" or "RUN"
+	Distance  int    // The distance to move or run
+	Direction string // The direction to move or run, e.g., "NORTH", "SOUTH", "EAST", "WEST"
+	X         int    // X coordinate for "WHAT IS AT"
+	Y         int    // Y coordinate for "WHAT IS AT"
+}
+
+var moveRunRule = ParserCore.ParseRule{
+	Name: "MoveOrRunRule",
+	Steps: []ParserCore.ParserRuleStep{
+		{
+			Name:            "MoveOrRun_Step1",
+			ParserType:      ParserCore.PARSE_STRING_CHOICE,
+			Options:         ParserCore.PARSE_OPTION_CONVER_TO_UPPERCASE,
+			ParsedValues:    []string{"MOVE", "RUN"}, // Accept either one
+			SkipOnTypeError: true,
+			ParseHandler: func(err error, token interface{}, tokType int, data *interface{}) (int, error) {
+				if err != nil {
+					// If we don't get a MOVE Or RUN, skip to the next possible rule
+					return ParserCore.SKIP_RULE_ON_ERROR, err
+				}
+				do := (*data).(*DataObject)
+				do.Command = token.(string)
+				return ParserCore.PARSE_SUCCESS, nil
+			},
+		},
+		{
+			Name:            "MoveOrRun_Step2",
+			ParserType:      ParserCore.PARSE_ANY_INTEGER,
+			SkipOnTypeError: false,
+			ParseHandler: func(err error, token interface{}, tokType int, data *interface{}) (int, error) {
+				do := (*data).(*DataObject)
+				do.Distance = token.(int)
+				return ParserCore.PARSE_SUCCESS, nil
+			},
+		},
+		{
+			Name:            "MoveOrRun_Step3",
+			ParserType:      ParserCore.PARSE_STRING_CHOICE,
+			Options:         ParserCore.PARSE_OPTION_CONVER_TO_UPPERCASE,
+			SkipOnTypeError: false,
+			ParsedValues:    []string{"NORTH", "SOUTH", "EAST", "WEST"},
+			ParseHandler: func(err error, token interface{}, tokType int, data *interface{}) (int, error) {
+				do := (*data).(*DataObject)
+				do.Direction = token.(string)
+				return ParserCore.PARSE_SUCCESS, nil
+			},
+		},
+	},
+}
+
+var WhatIsRule = ParserCore.ParseRule{
+	Name: "WhatIsRule",
+	Steps: []ParserCore.ParserRuleStep{
+		{
+			Name:            "WhatIs_Step1",
+			ParserType:      ParserCore.PARSE_STRING_LIST,
+			ParsedValues:    []string{"WHAT", "IS", "AT"},
+			Options:         ParserCore.PARSE_OPTION_CONVER_TO_UPPERCASE,
+			SkipOnTypeError: true, // If we don't get the expected string, skip to the next rule
+			ParseHandler: func(err error, token interface{}, tokType int, data *interface{}) (int, error) {
+				if err != nil {
+					// If we don't get the expected string, skip to the next rule
+					return ParserCore.SKIP_RULE_ON_ERROR, err
+				}
+				do := (*data).(*DataObject)
+				do.Command = "WHAT IS AT"
+				return ParserCore.PARSE_SUCCESS, nil
+			},
+		},
+		{
+			Name:            "WhatIs_Step2",
+			ParserType:      ParserCore.PARSE_ANY_INTEGER,
+			SkipOnTypeError: false,
+			ParseHandler: func(err error, token interface{}, tokType int, data *interface{}) (int, error) {
+				do := (*data).(*DataObject)
+				do.X = token.(int)
+				return ParserCore.PARSE_SUCCESS, nil
+			},
+		},
+		{
+			Name:            "WhatIs_Step3",
+			ParserType:      ParserCore.PARSE_COMMA,
+			SkipOnTypeError: false,
+			ParseHandler: func(err error, token interface{}, tokType int, data *interface{}) (int, error) {
+				if err != nil {
+					// If we don't get a comma, we can't continue
+					return ParserCore.SKIP_RULE_ON_ERROR, err
+				}
+				return ParserCore.PARSE_SUCCESS, nil
+			},
+		},
+		{
+			Name:            "WhatIs_Step4",
+			ParserType:      ParserCore.PARSE_ANY_INTEGER,
+			SkipOnTypeError: false,
+			ParseHandler: func(err error, token interface{}, tokType int, data *interface{}) (int, error) {
+				if err != nil {
+					// If we don't get an integer, we can't continue
+					return ParserCore.SKIP_RULE_ON_ERROR, err
+				}
+				do := (*data).(*DataObject)
+				do.Y = token.(int)
+				return ParserCore.PARSE_SUCCESS, nil
+			},
+		},
+	},
+}
+
+var Rules = []ParserCore.ParseRule{
+	moveRunRule,
+	WhatIsRule,
+}
--- main.go
+++ main.go
@@ -5,130 +5,17 @@ package main
 //
 import (
 	"ParserCombinator/ParserCombinator/ParserCore"
+	"ParserCombinator/ParserCombinator/Rulebase"
 )
 
-// DataObject is a structure that holds the parsed data we care about
-type DataObject struct {
-	Command   string
-	Distance  int
-	Direction string
-	XPos      int
-	YPos      int
-}
-
-// This is a list of rules that we can use to parse commands.
-var parserRules = []ParserCore.ParseRule{
-	{
-		//
-		// The MoveRunRule expects input of the form
-		// MOVE or RUN followed by an integer distance and a compass direction such as
-		// MOVE 12 SOUTH or RUN 5 EAST
-		//
-		Name: "MoveOrRunRule",
-		Steps: []ParserCore.ParserRuleStep{
-			{
-				Name:            "MoveOrRun_Step1",
-				ParserType:      ParserCore.PARSE_STRING_CHOICE,
-				Options:         ParserCore.PARSE_OPTION_CONVER_TO_UPPERCASE,
-				ParsedValues:    []string{"MOVE", "RUN"}, // Accept either one
-				SkipOnTypeError: true,
-				ParseHandler: func(err error, token interface{}, tokType int, data *interface{}) (int, error) {
-					if err != nil {
-						// If we don't get a MOVE Or RUN, skip to the next possible rule
-						return ParserCore.SKIP_RULE_ON_ERROR, err
-					}
-					do := (*data).(*DataObject)
-					do.Command = token.(string)
-					return ParserCore.PARSE_SUCCESS, nil
-				},
-			},
-			{
-				// Get the integer distance
-				Name:            "MoveOrRun_Step2",
-				ParserType:      ParserCore.PARSE_ANY_INTEGER,
-				SkipOnTypeError: false,
-				ParseHandler: func(err error, token interface{}, tokType int, data *interface{}) (int, error) {
-					do := (*data).(*DataObject)
-					do.Distance = token.(int)
-					return ParserCore.PARSE_SUCCESS, nil
-				},
-			},
-			{
-				// Get the direction, which is a string from a list of choices
-				Name:            "MoveOrRun_Step3",
-				ParserType:      ParserCore.PARSE_STRING_CHOICE,
-				Options:         ParserCore.PARSE_OPTION_CONVER_TO_UPPERCASE,
-				SkipOnTypeError: false,
-				ParsedValues:    []string{"NORTH", "SOUTH", "EAST", "WEST"},
-				ParseHandler: func(err error, token interface{}, tokType int, data *interface{}) (int, error) {
-					do := (*data).(*DataObject)
-					do.Direction = token.(string)
-					return ParserCore.PARSE_SUCCESS, nil
-				},
-			},
-		},
-	},
-	{
-		//
-		// The WhatIsATRule expects a phrase of the form:
-		// WHAT IS AT <X>,<Y> where <X> and <Y> are integers ex:
-		// WHAT IS AT 92, 12
-		//
-		Name: "WhatIsAtRule",
-		Steps: []ParserCore.ParserRuleStep{
-			{
-				Name:            "WhatIsAt_Step1",
-				ParserType:      ParserCore.PARSE_STRING_LIST,
-				Options:         ParserCore.PARSE_OPTION_CONVER_TO_UPPERCASE,
-				ParsedValues:    []string{"WHAT", "IS", "AT"},
-				SkipOnTypeError: true, // If we don't get the expected string, skip to the next rule
-				ParseHandler: func(err error, token interface{}, tokType int, data *interface{}) (int, error) {
-					(*data).(*DataObject).Command = "WHATIS"
-					return ParserCore.PARSE_SUCCESS, nil
-				},
-			},
-			{
-				// Get the X integer position
-				Name:            "WhatIsAt_Step2",
-				ParserType:      ParserCore.PARSE_ANY_INTEGER,
-				SkipOnTypeError: false, // If we don't get an integer, we can't continue
-				ParseHandler: func(err error, token interface{}, tokType int, data *interface{}) (int, error) {
-					(*data).(*DataObject).XPos = token.(int)
-					return ParserCore.PARSE_SUCCESS, nil
-				},
-			},
-			{
-				// Get the comma separator
-				Name:            "WhatIsAt_Step3",
-				ParserType:      ParserCore.PARSE_COMMA,
-				SkipOnTypeError: false, // If we don't get a comma, we can't continue
-				ParseHandler: func(err error, token interface{}, tokType int, data *interface{}) (int, error) {
-					return ParserCore.PARSE_SUCCESS, nil
-				},
-			},
-			{
-				// Get the Y integer position
-				Name:            "WhatIsAt_Step4",
-				SkipOnTypeError: false,
-				ParserType:      ParserCore.PARSE_ANY_INTEGER,
-				ParseHandler: func(err error, token interface{}, tokType int, data *interface{}) (int, error) {
-					(*data).(*DataObject).YPos = token.(int)
-					return ParserCore.PARSE_SUCCESS, nil
-				},
-			},
-		},
-	},
-}
-
 func main() {
-	do := DataObject{} // Create our special data object
-	_, err := ParserCore.Parse("What is at 153,54", parserRules, &do, true)
+	do := Rulebase.DataObject{} // Create our special data object
+	_, err := ParserCore.Parse("What is at 153,54", Rulebase.Rules, &do, true)
 	if err != nil {
 		println("Error parsing input:", err.Error())
 		return
 	} else {
 		println("Completed",
-			"[", do.Command, ":", do.Distance, ":", do.Direction,
-			" X:", do.XPos, " Y:", do.YPos, "]")
+			"[", do.Command, ":", do.Distance, ":", do.Direction, " X:", do.X, " Y:", do.Y, "]")
 	}
 }
