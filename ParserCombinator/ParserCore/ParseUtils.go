3eca8a46416b5003426cca42561b7df27f871c87
John Antypas <ja@antypas.net>
2025-07-16 11:53:37 -0700

    Moved code to objects rather than functions

--- ParserCombinator/ParserCore/ParseUtils.go
+++ ParserCombinator/ParserCore/ParseUtils.go
@@ -1,20 +1,5 @@
 package ParserCore
 
-import (
-	"fmt"
-	"strings"
-)
-
-func (p *ParserObject) StripInputNoise(striptokens []string) {
-	// This function strips unwanted tokens from the input string
-	for _, token := range striptokens {
-		p.Input = strings.ReplaceAll(p.Input, token, "")
-		if p.Debug {
-			fmt.Println("Stripped token:", token, "leaving", p.Input)
-		}
-	}
-}
-
 func (p *ParserObject) SetDebug(state bool) {
 	p.Debug = state
 }
